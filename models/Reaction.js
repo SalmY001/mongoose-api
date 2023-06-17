791edb733400e7b1e2d37b21e9fcb264e36e3227
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-06-17 19:35:07 +0100

    add reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionID: {_id: Schema.Types.ObjectId },
+  createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true, trim: true },
+  reactionBody: { type: String, minlength: 1, maxlength: 280 , required: true, trim: true }
+},
+{
+  toJSON: {
+    //Add a `virtuals` property and set it's value to true
+    virtuals: true,
+  },
+  id: false,
+}
+);
+
+// Create a virtual property `friendCount` that records the number of friends
+// reactionSchema
+//   .virtual('reactionCount')
+//   // Getter
+//   .get(function () {
+//     return this.reactions.length;
+//    // return `${this.reactions.length}`;
+
+//   });
+
+// Initialize the Thoughts model
+
+const Reaction = model('reaction', reactionSchema);
+
+const handleError = (err) => console.error(err);
